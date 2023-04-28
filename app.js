e7f4d728f970c5860b848100d22c6d34fd8802ea
54albert54 <user.name>
2023-04-28 04:44:14 -0400

    muchos cambios para la version movil

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
 const puntuaje = document.getElementById('puntuaje')
 const time = document.getElementById('time')
-const velocidad = document.getElementById('velocidad')
+const divGameOver = document.querySelector('.gameOver')
+const botonera = document.querySelector('.botones')
+const inicio = document.querySelector('.gameStar')
+const opciones = document.querySelector('.opciones')
+
+
+
 
 canvas.width = 64 * 18;  //32
 canvas.height = 64 * 32;  //18
@@ -13,9 +19,7 @@ imVerde.src = flechaverde
 const flecharoja = "./assets/flechaRojo.png";
 const imRojo = new Image();
 imRojo.src = flecharoja
-const neutra = "./assets/Untitled-1.png";
-const imgneutra = new Image();
-imgneutra.src = flecharoja
+
 const flechaazul = "./assets/flechaAzul.png";
 const imAzul = new Image();
 imAzul.src = flechaazul
@@ -24,16 +28,31 @@ const flechaamarilla = "./assets/flechaAmarillo.png";
 const flechaamarilla2 = "./assets/flechaAmarillo2.png";
 const imAmarillo = new Image();
 imAmarillo.src = flechaamarilla
-let altura = 300;
-const hightAmarillo = altura + 0
-const hightRojo = altura + 150;
-const hightVerde = altura + 300
-const hightAzul = altura + 450
-let rangoJuego = 1700; //la idea es poder mover esto en un futuro
-// let enemySpeed = 7
+let altura = -50;
+let anchura = 100
+const yAmarillo = altura //+ 0
+const yRojo = altura//+ 150;
+const yVerde = altura// + 300
+const yAzul = altura// + 450
+
+
+
+
+const xAmarillo = anchura + 300
+const xRojo = anchura + 0;
+const xVerde = anchura + 870
+const xAzul = anchura + 600
+
+
+let yJuego = canvas.height - 150;
+
+
+
+let rangoJuego = 1700;
+
 let makePoint = true
-let gameOver = false;
-let segundos = 0;
+let gameOver = true;
+let segundos = -1;
 let botones = []
 let flechas = [];
 
@@ -46,20 +65,12 @@ class Game {
     this.enemyTime = 0
     this.enemyInterval = 1000
     this.time = 0;
-    this.maxTime = 60000;
+    this.maxTime = 5;
     this.score = 0;
     this.backGround = new Image()
     this.backGround.src = "./assets/wallpaper.png"
   };
   draw(c) {
-    c.drawImage(this.backGround, 0, 0)
-
-    flechas.push(new Flecha(0, hightAzul, flechaazul))
-    flechas.push(new Flecha(0, hightAmarillo, flechaamarilla))
-    flechas.push(new Flecha(0, hightRojo, flecharoja))
-    flechas.push(new Flecha(0, hightVerde, flechaverde))
-
-
 
   }
   update(deltatime) {
@@ -69,12 +80,13 @@ class Game {
 
     // apuntadores hacia HTML
     puntuaje.innerText = this.score
-    // time.innerText = this.time
-    velocidad.innerText = this.enemySpeed
-    this.time += deltatime
-    if (this.time > this.maxTime) {
+
+
+
+    if (segundos >= this.maxTime) {
       gameOver = true,
         mensajeGameOver()
+
     }
 
 
@@ -82,16 +94,12 @@ class Game {
   update2(deltatime) {
 
     if (this.enemyTime > this.enemyInterval) {
-      // this.addEnemy();
+      this.addEnemy();
       this.enemyTime = 0;
     } else {
       this.enemyTime += deltatime;
     }
     flechas = flechas.filter(flechas => !flechas.markForDelete)
-
-    // aqui estaba un modulo que no era necesario para nada :D a verd que otro cambio hag antes de romperlo
-
-
   };
   addEnemy() {
     Math.random() < 0.4 ? nuevaFlechaVerde() : this.addEnemy2()
@@ -110,55 +118,66 @@ setInterval(function () {
   time.innerHTML = segundos + ' segundos';
 }, 1000);
 
-const mensajeGameOver = () => {
 
-  c.fillStyle = 'white'
-  c.fillRect(0, 0, canvas.width, canvas.height)
-  c.fillStyle = "black"
-  c.font = "150px serif";
-  c.fillText("Game Over ", canvas.width / 4, 200);
-  c.font = "50px serif";
-  c.fillText(`tu puntaje fue de ${game.score}`, canvas.width / 4, 250);
-}
 
 
 const nuevaFlechaVerde = () => {
-  flechas.push(new Flecha(0, hightVerde, flechaverde))
+  flechas.push(new Flecha(xVerde, yVerde, flechaverde))
 }
 const nuevaFlechaRoja = () => {
-  flechas.push(new Flecha(0, hightRojo, flecharoja))
+  flechas.push(new Flecha(xRojo, yRojo, flecharoja))
 }
 const nuevaFlechaAmarilla = () => {
-  flechas.push(new Flecha(0, hightAmarillo, flechaamarilla))
+  flechas.push(new Flecha(xAmarillo, yAmarillo, flechaamarilla))
 }
 const nuevaFlechaAzul = () => {
-  f
-  lechas.push(new Flecha(0, hightAzul, flechaazul))
+  flechas.push(new Flecha(xAzul, yAzul, flechaazul))
 }
 
 const dificultad = () => {
-  switch (game.score) {
+  switch (segundos) {
     case 5:
       game.enemySpeed = 6.5
+      game.enemyInterval = 950
       break
     case 10:
       game.enemySpeed = 7
+      game.enemyInterval = 800
       break
     case 15:
-      game.enemySpeed = 9.5
-      game.enemyInterval = 900
+      game.enemySpeed = 8.5
+      game.enemyInterval = 850
       break
     case 20:
-      game.enemySpeed = 11
+      game.enemySpeed = 10
       game.enemyInterval = 750
       break
     case 25:
-      game.enemySpeed = 14.5
+      game.enemySpeed = 11.5
       game.enemyInterval = 500
       break
     case 30:
-      game.enemySpeed = 15.5
+      game.enemySpeed = 12
       game.enemyInterval = 600
+    case 35:
+      game.enemySpeed = 12.5
+      game.enemyInterval = 500
+      break
+    case 40:
+      game.enemySpeed = 13
+      game.enemyInterval = 400
+      break
+    case 45:
+      game.enemySpeed = 13.5
+      game.enemyInterval = 350
+      break
+    case 50:
+      game.enemySpeed = 14
+      game.enemyInterval = 300
+      break
+    case 55:
+      game.enemySpeed = 15
+      game.enemyInterval = 100
       break
 
   }
@@ -175,13 +194,17 @@ class Player {
     this.keys = [];
   };
   draw(c) {
+    c.fillStyle = "black"
+    c.font = '80px Silkscreen'
+    c.fillText('TIEMPO ', canvas.width / 3, 80);
+    c.fillText(segundos, 510, 157);
 
-    c.strokeRect(this.ArrowUP.x, altura, this.ArrowUP.width, this.ArrowUP.height)
-    c.strokeRect(this.ArrowUP.x - 100, altura, this.ArrowUP.width, this.ArrowUP.height)
-    c.drawImage(imVerde, rangoJuego, hightVerde, 100, 100)
-    c.drawImage(imRojo, rangoJuego, hightRojo, 100, 100)
-    c.drawImage(imAzul, rangoJuego, hightAzul, 100, 100)
-    c.drawImage(imAmarillo, rangoJuego, hightAmarillo, 100, 100)
+
+
+    c.drawImage(imVerde, xVerde, yJuego, 100, 100)
+    c.drawImage(imRojo, xRojo, yJuego, 100, 100)
+    c.drawImage(imAzul, xAzul, yJuego, 100, 100)
+    c.drawImage(imAmarillo, xAmarillo, yJuego, 100, 100)
 
 
   }
@@ -238,10 +261,10 @@ class Player {
   acctionKey() {
     this.input()
 
-    if (this.keys == 'ArrowRight') pressButon(rangoJuego, hightVerde, flechaverde), this.colitionArrow()
-    else if (this.keys == 'ArrowLeft') pressButon(rangoJuego, hightRojo, flecharoja), this.colitionArrow()
-    else if (this.keys == 'ArrowDown') pressButon(rangoJuego, hightAzul, flechaazul), this.colitionArrow()
-    else if (this.keys == 'ArrowUp') pressButon(rangoJuego, hightAmarillo, flechaamarilla), this.colitionArrow()
+    if (this.keys == 'ArrowRight') pressButon(xVerde, yJuego, flechaverde), this.colitionArrow()
+    else if (this.keys == 'ArrowLeft') pressButon(xRojo, yJuego, flecharoja), this.colitionArrow()
+    else if (this.keys == 'ArrowDown') pressButon(xAzul, yJuego, flechaazul), this.colitionArrow()
+    else if (this.keys == 'ArrowUp') pressButon(xAmarillo, yJuego, flechaamarilla), this.colitionArrow()
 
   }
   colitionArrow() {
@@ -268,11 +291,28 @@ class Player {
   }
 
 }
+
+// funciones del HTML
+const gameStart = () => {
+  inicio.classList.add('noMostrar')
+  botonera.classList.remove('noMostrar'), animate(0);
+}
+
+const showopciones = () => { opciones.classList.add('mostrar'), opciones.classList.remove('noMostrar') }
+const shangeTime = (t) => {
+  game.maxTime = t, opciones.classList.add('noMostrar');
+}
 const releaseButons = () => { player.keys = [], botones = [] }
 const pushArriba = () => { player.keys.push('ArrowUp') }
 const pushAbajo = () => { player.keys.push('ArrowDown') }
 const pushDerecha = () => { player.keys.push('ArrowRight') }
 const pushIzquierda = () => { player.keys.push('ArrowLeft') }
+const mensajeGameOver = () => {
+  divGameOver.classList.add('mostrar')
+
+  divGameOver.classList.remove('noMostrar')
+
+}
 
 class PressButton {
   constructor(x, y, imagen) {
@@ -308,6 +348,7 @@ class Flecha {
     this.height = 100;
     this.markForDelete = false;
     this.newArrowGreen = 0
+    this.crecer = .25
 
   }
   draw() {
@@ -315,23 +356,82 @@ class Flecha {
     c.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
   update() {
-    this.x += game.enemySpeed;
-    if (this.x > canvas.width) {
+    this.y += game.enemySpeed;
+    // this.x -= .4
+    // this.width += this.crecer
+    // this.height += this.crecer
+    if (this.y > canvas.height) {
       this.markForDelete = true
     }
   }
 }
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.image = image
+    this.width = 2244
+    this.height = 2048
+    this.speedModifier = speedModifier
+  }
+  update() {
+    if (this.x < -this.width) this.x = 0;
+    else this.x -= this.speedModifier * (game.enemySpeed * 0.2)
+
+  }
+  draw() {
+    c.drawImage(this.image, this.x, this.y, this.width, this.height)
+    c.drawImage(this.image, this.x + this.width - this.speedModifier, this.y, this.width, this.height)
+
+
+  }
+}
+
+class BackGround {
+  constructor() {
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.imagelayer1 = layer1
+    this.imagelayer2 = layer2
+    this.imagelayer3 = layer3
+    this.imagelayer4 = layer4
+    this.layer1 = new Layer(this.imagelayer1, 0.8)
+    this.layer2 = new Layer(this.imagelayer2, 0.4)
+    this.layer3 = new Layer(this.imagelayer3, 0.6)
+    this.layer4 = new Layer(this.imagelayer4, 0)  // este es el fondo azul detras no es necesario qu este moviendo
+
+    this.bGArray = [this.layer4, this.layer2, this.layer3, this.layer1]
+
+  }
+  update() {
+    this.bGArray.forEach(Layer => {
+      Layer.update()
+    })
+  }
+  draw() {
+    this.bGArray.forEach(Layer => {
+      Layer.draw()
+
+    })
+  }
+}
+
+let backGround = new BackGround()
+
 const game = new Game()
 const player = new Player(rangoJuego)
 let lastime = 0
 const animate = (timeStamp) => {
   const deltatime = timeStamp - lastime;
   lastime = timeStamp
-
+  gameOver = false
 
   c.fillStyle = " white"
   c.fillRect(0, 0, canvas.width, canvas.height)
+  backGround.draw()
+  backGround.update()
   game.draw(c)
+
   c.save()
   c.globalAlpha = 0.3
   player.draw(c)
@@ -356,4 +456,4 @@ const animate = (timeStamp) => {
     requestAnimationFrame(animate)
   }
 };
-animate(0)
+// animate(0)
